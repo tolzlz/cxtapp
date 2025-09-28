ab3de3e9802e5d0636f081fc23fcb4757248b696
tolzlz0636 <tolzlz0636@gmail.com>
2025-09-28 09:17:38 -0700

    协程网络

--- network/src/main/java/com/cxtapp/network/scope/Scope.kt
+++ network/src/main/java/com/cxtapp/network/scope/Scope.kt
@@ -3,60 +3,73 @@ package com.cxtapp.network.scope
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
-import com.cxtapp.network.extensions.cancelScopeOn
+import com.drake.net.utils.runMain
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
-class BaseNetScope(lifecycleOwner: LifecycleOwner? = null,
-                   lifeEvent: Lifecycle.Event = Lifecycle.Event.ON_DESTROY,
-                   val dispatcher: CoroutineDispatcher = Dispatchers.Main) : CoroutineScope {
+abstract class Scope(lifecycleOwner: LifecycleOwner? = null,
+                 lifeEvent: Lifecycle.Event = Lifecycle.Event.ON_DESTROY,
+                 val dispatcher: CoroutineDispatcher = Dispatchers.Main) : CoroutineScope {
 
 
     init {
-        lifecycleOwner?.lifecycle?.addObserver(object : LifecycleEventObserver {
-            override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
-                if (lifeEvent == event) cancel()
-            }
-        })
+        runMain {
+            lifecycleOwner?.lifecycle?.addObserver(object : LifecycleEventObserver {
+                override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+                    if (lifeEvent == event) cancel()
+                }
+            })
+        }
     }
 
     private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
         catch(throwable)
     }
 
-    protected var catch: (BaseNetScope.(Throwable) -> Unit)? = null
-    protected var finally: (BaseNetScope.(Throwable?) -> Unit)? = null
+    protected var catch: (Scope.(Throwable) -> Unit)? = null
+    protected var finally: (Scope.(Throwable?) -> Unit)? = null
 
 
     override val coroutineContext: CoroutineContext =
         dispatcher + exceptionHandler + SupervisorJob()
 
+
+    open fun launch(block: suspend CoroutineScope.() -> Unit): Scope {
+        launch(EmptyCoroutineContext) {
+            block()
+        }.invokeOnCompletion {
+            finally(it)
+        }
+        return this
+    }
     protected fun catch(e: Throwable) {
-        catch?.invoke(this@BaseNetScope,e) ?: handleError(e)
+        catch?.invoke(this@Scope,e) ?: handleError(e)
     }
     /**
      * @param e 如果发生异常导致作用域执行完毕, 则该参数为该异常对象, 正常结束则为null
      */
     protected fun finally(e: Throwable?) {
-        finally?.invoke(this@BaseNetScope,e)
+        finally?.invoke(this@Scope,e)
     }
 
     /**
      * 当作用域内发生异常时回调
      */
-    open fun catch(block: BaseNetScope.(Throwable) -> Unit = {}): BaseNetScope {
+    open fun catch(block: Scope.(Throwable) -> Unit = {}): Scope {
         this.catch  = block
         return this
     }
     /**
      * 无论正常或者异常结束都将最终执行
      */
-    open fun finally(block: BaseNetScope.(Throwable?) -> Unit = {}): BaseNetScope {
+    open fun finally(block: Scope.(Throwable?) -> Unit = {}): Scope {
         this.finally = block
         return this
     }
